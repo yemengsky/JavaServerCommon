ccc85a8c43c3cc3381f44a0006993b07be7174ba
yemengsky <yemengsky@gmail.com>
2016-07-29 13:26:09 +0800

    Create LogUtil.java

---
+++ LogUtil.java
@@ -0,0 +1,17 @@
+public class LogUtil {
+    public static void log(Object... objects) {
+        StringBuffer sb = new StringBuffer(getString());
+        for (Object object : objects) {
+            sb.append(" ");
+            sb.append(((object == null) ? "NULL" : object.toString()));
+        }
+        System.out.println(sb);
+    }
+
+    private static String getString() {
+        StackTraceElement ste = new Throwable().getStackTrace()[2];
+        String valueString = ste.getFileName().replace(".java", "") + " "
+                + ste.getMethodName() + ":" + ste.getLineNumber() + " ";
+        return valueString;
+    }
+}
